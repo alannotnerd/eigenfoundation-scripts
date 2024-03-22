1511c552146e565c92bcc90ee014f9919f09d7fe
Gas <86567384+gas1cent@users.noreply.github.com>
2024-03-22 14:00:56 +0400

    feat: install linters (#4)

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["data", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+};
