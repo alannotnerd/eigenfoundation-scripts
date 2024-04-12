df0078c391df27910b709e0e5cbaf840672d14cf
0xkenj1 <165053496+0xkenj1@users.noreply.github.com>
2024-04-12 10:57:49 -0300

    feat: add github actions (#19)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,11 @@ module.exports = {
   root: true,
   env: { es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["data", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+  ignorePatterns: ['data', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
 };
